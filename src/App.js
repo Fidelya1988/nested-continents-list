eedb8dda9505420fa3a35a644c8ccc3cc9379f25
Fidelya1988 <oleksandram1988@gmail.com>
2021-07-05 00:57:26 +0300

    deleted needles code

--- src/App.js
+++ src/App.js
@@ -8,31 +8,7 @@ function App() {
   const { data, loading, error } = useQuery(GET_CONTINENTS);
   const [selectedContinent, setSelectedContinent] = useState(null);
   const [selectedCountry, setSelectedCountry] = useState(null);
-  // const [continents, setContinents] = useState();
 
-  const handleContinentSelect = (code) => {
-    if (selectedContinent === code) {
-      setSelectedContinent(null);
-    } else {
-      setSelectedContinent(code);
-    }
-  };
-
-  const handleCountrySelect = (code) => {
-    if (selectedCountry === code) {
-      setSelectedCountry(null);
-    } else {
-      setSelectedCountry(code);
-    }
-  };
-
-  // const isContinentCode = data.continents.sort(
-  //   (continent) => continent.code === code
-  //  );
-
-  //   const isCountryCode = data.continents.countries.sort(
-  //    (country) => country.code === code
-  //   );
   const getContinentCode = (code) => {
     const codes = data.continents.map((continent) => continent.code);
 
@@ -65,33 +41,6 @@ function App() {
             selectedItem={{ selectedContinent, selectedCountry }}
             handleSelect={handleSelect}
           />
-          {/* {data.continents.map(({ name, code, countries }) => (
-            <li
-              key={code}
-              className={selectedContinent === code ? styles.selected : ""}
-            >
-              <p onClick={() => handleContinentSelect(code)}>{name}</p>
-              {selectedContinent === code && (
-                <ul>
-                  {countries.map((country) => (
-                    <li
-                      key={country.code}
-                      onClick={() => handleCountrySelect(country.code)}
-                    >
-                      <p>{country.name}</p>
-                      {selectedCountry === country.code && (
-                        <ul>
-                          {country.languages?.map((language) => (
-                            <li key={language.code}>{language.name}</li>
-                          ))}
-                        </ul>
-                      )}
-                    </li>
-                  ))}
-                </ul>
-              )}
-            </li>
-          ))} */}
         </ul>
       ) : (
         <Loader />
