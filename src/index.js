36e44341844f0eb3bfe1bb2fc5042d84d178f9de
Fidelya1988 <oleksandram1988@gmail.com>
2021-07-05 04:16:33 +0300

    created helpers.context

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import {ApolloProvider} from "@apollo/client";
-import {client} from "./api/client/client";
-
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ApolloProvider } from "@apollo/client";
+import { client } from "./api/client/client";
+import { getChildArray } from "./helpers/getChildArray";
+import { getSelectedItem } from "./helpers/getSelectedItem";
+export const HelpersContext = React.createContext({
+  getChildArray,
+  getSelectedItem,
+});
 ReactDOM.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <App />
+      <HelpersContext.Provider value={{ getChildArray, getSelectedItem }}>
+        <App />
+      </HelpersContext.Provider>
     </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
-
